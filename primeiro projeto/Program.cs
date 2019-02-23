79575f96280a1b03297e12ed5579f2b155ccec2c
Paulo <paulo.fgermano@gmail.com>
2019-02-23 14:32:20 -0300

    Adicionei bom dia!

---
+++ primeiro projeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!!");
+        }
+    }
+}
